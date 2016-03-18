5d9fe5d60e586bdc3160e153297823801063a66b
Wanny Miarelli <wannymiarelli@live.com>
2016-03-18 10:35:06 +0100

    first push

---
+++ src/main.js
@@ -0,0 +1,91 @@
+import express from 'express'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+import serveFavicon from 'serve-favicon'
+import {MongoClient} from 'mongodb'
+import request from 'request';
+import cheerio from 'cheerio';
+import nexmo from 'easynexmo';
+
+let app = express();
+app.use(serveFavicon(__dirname + '/public/favicon.ico'));
+app.use(express.static('lib/public/assets'));
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'jade');
+nexmo.initialize('79f0b103', '2947cd56', true);
+/*
+Database
+*/
+let servers = null;
+let failConsecutive = 0;
+//mongoose.connect("mongodb://brother:hermes2015@ds015879.mlab.com:15879/hermes-status");
+var dbUri = 'mongodb://brother:hermes2015@ds015879.mlab.com:15879/hermes-status'
+MongoClient.connect(dbUri, function(err, db){
+  if(err) console.error(err);
+  console.log('Connected to the database Hermes-Status');
+  servers  = db.collection('servers');
+  /*
+  Search for a server document with the given name, we actually manage only one server
+  */
+  servers.find({name: 'HermesHotels Central Data'}).toArray(function(err, data){
+    if(err) console.error(err);
+    if(data.length <= 0){
+      servers.insert({
+        name: 'HermesHotels Central Data',
+        status: true,
+        fail: 0,
+        success: 0,
+        lastCheck: new Date()
+      });
+    }
+  });
+  /*
+  Check the connection every 2 minutes
+  */
+  checkHermes(db, servers);
+  setTimeout(function(){
+    checkHermes(db, servers);
+  }, 120000);
+});
+
+/*
+Server check every 2 minutes
+*/
+function checkHermes(db, servers){
+  console.log('Starting server check');
+  request({
+    uri: 'https://secure.hermeshotels.com/bol/dispo.do?caId=138&hoId=1311'
+  }, function(error, response, body){
+    var status = body.indexOf('HOTEL CHIMERA');
+    if(status === -1){
+      //Server down send notification
+      nexmo.sendTextMessage('HermesBigBr', '+393888108490', 'HH - Errore di connessione');
+      nexmo.sendTextMessage('HermesBigBr', '+393485592637', 'HH - Errore di connessione');
+      nexmo.sendTextMessage('HermesBigBr', '+393402485276', 'HH - Errore di connessione');
+      console.log('Server down ' + new Date());
+      failConsecutive = failConsecutive + 1;
+      if(failConsecutive > 5){
+        //Make voice call to Romano
+        nexmo.sendTTSMessage('+393888108490', 'Rilevato errore connessione HermesHotels');
+      }
+      //Find the document and increment it by 1
+      servers.findOneAndUpdate({name: 'HermesHotels Central Data'}, {$inc: {fail: +1}, $set: {status: false}});
+    }else{
+      servers.findOneAndUpdate({name: 'HermesHotels Central Data'}, {$inc: {success: +1}, $set: {status: true}});
+    }
+  });
+}
+
+/*
+Routes
+*/
+app.get('/', function(req, res){
+  var server = servers.findOne({name: 'HermesHotels Central Data'}, function(err, doc){
+    if(err) throw err;
+    res.render('index', {server: doc});
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
